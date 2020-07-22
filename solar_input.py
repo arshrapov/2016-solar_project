11b5cfed4174b244033ada005c43f1295e937434
Raphael <ashrapov.rafa@yandex.com>
2020-07-22 11:57:40 +0500

    Function slace_y now is working and now functions parse_star_parameters and parse_planet_parameters is working

--- solar_input.py
+++ solar_input.py
@@ -44,7 +44,16 @@ def parse_star_parameters(line, star):
     **star** — объект звезды.
     """
 
-    pass  # FIXME: not done yet
+    params = line.split()
+
+    star.R = params[1]
+    star.color = params[2]
+    star.m = params[3]
+    star.x = params[4]
+    star.y = params[5]
+    star.Vx = params[6]
+    star.Vy = params[7]
+
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +70,16 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+
+    params = line.split()
+
+    planet.R = params[1]
+    planet.color = params[2]
+    planet.m = params[3]
+    planet.x = params[4]
+    planet.y = params[5]
+    planet.Vx = params[6]
+    planet.Vy = params[7]
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
